edee31426d31cea0953732b13be5d8c8ad5a1425
Michael Rascati <rascatimichael@gmail.com>
2019-02-25 18:03:04 -0500

    images now referenced correctly with require

--- app/src/components/Gallery/Gallery.js
+++ app/src/components/Gallery/Gallery.js
@@ -1,28 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-// '../../assets/images/'
-
-
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const images = importAll(require.context('../../assets/images/', false, /\.(png|jpe?g|svg)$/));
-export { images }
-// const images = importAll(require.context('../../../build/static/media', false, /\.(png|jpe?g|svg)$/));
-
-// import bear from '../../assets/images/bear.jpg'
-// import cat from '../../assets/images/cat.jpg'
-// import corn from '../../assets/images/corn.jpg'
-// import harp from '../../assets/images/harp.jpg'
-// import horse from '../../assets/images/horse.jpg'
-// import hotdogs from '../../assets/images/hotdogs.jpg'
-// import lion from '../../assets/images/lion.jpg'
-// import meerkat from '../../assets/images/meerkat.jpg'
-// import mushroom from '../../assets/images/mushroom.jpg'
-// import plant from '../../assets/images/plant.jpg'
-// import puppies from '../../assets/images/puppies.jpg'
-// import tiger from '../../assets/images/tiger.jpg'
 
 
 const ImageGrid = styled.div`
@@ -38,21 +15,35 @@ const Image = styled.div`
   }
 `
 
+const imageArray = [
+  "bear",
+  "cat",
+  "corn",
+  "harp",
+  "horse",
+  "hotdogs",
+  "lion",
+  "meerkat",
+  "mushroom",
+  "plant",
+  "puppies",
+  "tiger"
+]
+
 class Gallery extends Component {
   render() {
+    
     return (
-      // <img src={bear} alt={"Gallery image #"}/>
-
       <ImageGrid>
-        {images.map(( img, i ) => {
-          return (
-
-            <Image key={i}>
-              <img src={img} alt={"Gallery image #" + i}/>
+        {
+          imageArray.map(image => (
+            <Image key={image}>
+              <img src={require(`../../assets/images/${image}.jpg`)} alt="Gallery image" />
             </Image>
-
+            )
           )
-        })}
+        }
+
       </ImageGrid>
     )
   }
