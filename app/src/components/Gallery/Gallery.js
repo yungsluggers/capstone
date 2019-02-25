1c556a7dc105886580230e64c605a0147cb09610
Michael Rascati <rascatimichael@gmail.com>
2019-02-25 16:54:58 -0500

    adjust image pathing in gallery to work on the server

--- app/src/components/Gallery/Gallery.js
+++ app/src/components/Gallery/Gallery.js
@@ -6,7 +6,8 @@ function importAll(r) {
   return r.keys().map(r);
 }
 
-const images = importAll(require.context('../../assets/images/', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('/assets/images/', false, /\.(png|jpe?g|svg)$/));
+// const images = importAll(require.context('../../../build/static/media', false, /\.(png|jpe?g|svg)$/));
 
 const ImageGrid = styled.div`
   display: grid;
