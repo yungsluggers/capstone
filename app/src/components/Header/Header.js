e7912807647ab0412a5758feb1fc8c1167fa5c41
Michael Rascati <rascatimichael@gmail.com>
2019-02-26 22:51:02 -0500

    initial new styles for the main header and sections

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -3,56 +3,132 @@ import styled from 'styled-components'
 
 import { ReactComponent as HelpIcon } from '../../assets/icons/baseline-help_outline-24px.svg'
 
+import Button from '../Button/Button'
 
-const HeaderContainer = styled.div`
-  position: relative;
-  height: 45vh;
-  background-color: ${props => props.theme.green };
-  color: white;
-  box-shadow: 2px 2px 15px 0px rgba(0,0,0,0.2);
+const Container = styled.div`
+  /* position: relative; */
+  /* height: 45vh; */
+  /* background-color: ${props => props.theme.green }; */
+  /* color: white; */
+  /* box-shadow: 2px 2px 15px 0px rgba(0,0,0,0.2); */
   /* background-size: 200% auto; */
-  background-image: linear-gradient(120deg, rgba(65,185,132,1) 0%, rgba(72,193,139,1) 43%, rgba(84,206,151,1) 68%, rgba(95,219,162,1) 80%, rgba(99,218,162,1) 100%);
-  z-index: 1;
+  /* background-image: linear-gradient(120deg, rgba(65,185,132,1) 0%, rgba(72,193,139,1) 43%, rgba(84,206,151,1) 68%, rgba(95,219,162,1) 80%, rgba(99,218,162,1) 100%); */
+  /* z-index: 1; */
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto auto auto;
 `
 
-const HeaderTitle = styled.h3`
-  color: white;
-  text-align: left;
-  font-size: 1.875rem;
-  letter-spacing: 10px;
-  font-kerning: auto;
-`
+const Icon = styled.a`
+  grid-column: 1;
+  grid-row: 1;
 
-const HeaderContent = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const HeaderIcon = styled.a`
   svg {
     width: 45px;
     height: auto;
   }
 `
 
+const Title = styled.div`
+  grid-column: 3 / span 3;
+  grid-row: 2;
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  width: 100%;
+
+  h1 {
+    font-size: 7.5rem;
+    letter-spacing: .2rem;
+    /* display: inline; */
+    hyphens: auto;
+  }
+
+  span {
+    /* display: inline-block; */
+    background-color: ${props => props.theme.textPrimary};
+    color: white;
+    width: 100%;
+  }
+`
+
+const Subtitle = styled.div`
+  grid-column: 3 / span 3;
+  grid-row: 3;
+  text-transform: lowercase;
+
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  width: 100%;
+
+  span  {
+    background-color: ${props => props.theme.textPrimary};
+    color: white;
+    width: 100%;
+  }
+`
+
+const Content = styled.div`
+  grid-column: 3;
+  grid-row: 4;
+  /* display: flex; */
+  /* flex-flow: row nowrap; */
+  /* justify-content: space-between; */
+  /* align-items: center; */
+`
+
+const Names = styled.div`
+  grid-column: 1;
+  grid-row: 3;
+  text-transform: lowercase;
+  transform: rotate(-90deg);
+`
+
+
 class Header extends Component {
   render() {
     return (
-      <HeaderContainer>
-        
-        <HeaderContent className="l-container">
-          <HeaderTitle>
-            {this.props.children}
-          </HeaderTitle>
+      <Container>
+        <Title>
+          {/* {this.props.children} */}
+          <h1><span>{this.props.children}</span></h1>
+          {/* <h1><span><br />saur</span></h1> */}
+          {/* <h1>{this.props.children}</h1> */}
+        </Title>
 
-          <HeaderIcon>
-            <HelpIcon fill="white"/>
-          </HeaderIcon>
-        </HeaderContent>
+        <Subtitle>
+          <span>Abstract art generator</span>
+        </Subtitle>
+
+        <Icon>
+          <HelpIcon />
+        </Icon>
+
+        <Names>
+          Michael Rascati <br />
+          Kevin Altschuler <br />
+          Chris Louie <br />
+        </Names>
+
+        <Content>
+          <Button filled>
+            Get Started
+          </Button>
+        </Content>
         
-      </HeaderContainer>
+        {/* <Content className="l-container">
+          <Title>
+            {this.props.children}
+          </Title>
+
+          <Icon>
+            <HelpIcon fill="white"/>
+          </Icon>
+        </Content> */}
+        
+      </Container>
     )
   }
 }
--- app/src/components/Section/Section.js
+++ app/src/components/Section/Section.js
@@ -3,40 +3,82 @@ import styled from 'styled-components'
 
 
 const Container = styled.div`
-  background: ${props => props.green ? props.theme.green : 'white'};
-  padding: 3em 0em 7em 0em;
-  z-index: 0;
-  position: relative;
+  padding: 4em 0em;
   font-weight: lighter;
 `
 
-const SectionContent = styled.div`
-  color: ${props => props.green ? 'white' : props.theme.textPrimary};
+const Content = styled.div`
+  /* color: ${props => props.green ? 'white' : props.theme.textPrimary};
   text-align: left;
   font-size: 1.125rem;
   max-width: 66vw;
-  margin: 0 auto;
+  margin: 0 auto; */
+`
+
+
+const HeaderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  overflow: hidden;
 `
 
 const SectionHeader = styled.h1`
-  color: ${props => props.green ? 'white' : props.theme.green};
+  color: ${props => props.theme.textPrimary};
   text-transform: uppercase;
   text-align: center;
-  font-size: 3rem;
+  font-size: 4.5rem;
+
+  grid-column: 2;
+  grid-row: 1;
 `
+
+const OutlineHeader = styled.h1`
+  -webkit-text-stroke: 1px ${props => props.theme.textPrimary};
+  color: rgba(0, 0, 0, 0);
+  text-transform: uppercase;
+  text-align: center;
+  font-size: 4.5rem;
+  position: relative;
+  bottom: 20px;
+  left: ${props => props.from === 'left' ? '-22px' : '22px'};
+  grid-column: 2;
+  grid-row: 1;
+
+`
+
+
+const Underline = styled.div`
+  border-bottom: 10px solid ${props => props.theme.textPrimary};
+  grid-column: ${props => props.from === 'left' ? '1 / span 2' : '2 / span 2'};
+  grid-row: 1;
+  position: relative;
+  bottom: 48px;
+  left: ${props => props.from === 'left' ? '-30px' : '60px'};
+`
+
 
 class Section extends Component {
   render() {
+    
     return (
-      <Container green={this.props.green}>
+      
+      <Container >
+        <HeaderContainer from={this.props.from}>
+          
+          <SectionHeader>
+            {this.props.title}
+          </SectionHeader>
 
-        <SectionHeader green={this.props.green} className={this.props.narrow ? 'l-container-narrow' : null}>
-          {this.props.title}
-        </SectionHeader>
+          <OutlineHeader from={this.props.from}>
+            {this.props.title}
+          </OutlineHeader>
+          
+          <Underline from={this.props.from}/>
+        </HeaderContainer>
 
-        <SectionContent green={this.props.green} className={this.props.narrow ? 'l-container-narrow' : null}>
+        <Content className={this.props.narrow ? 'l-container-narrow' : null}>
           {this.props.children}
-        </SectionContent>
+        </Content>
 
       </Container>
     )
