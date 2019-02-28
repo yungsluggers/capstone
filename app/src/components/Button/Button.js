b8637a632efbd405291219ba26b34a4f281acf37
Michael Rascati <rascatimichael@gmail.com>
2019-02-27 22:17:47 -0500

    added button hover states

--- app/src/components/Button/Button.js
+++ app/src/components/Button/Button.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Button = styled.button`
+const Button = styled.button`
   color: ${props => props.filled ? 'white' : props.theme.textPrimary};
   background-color: ${props => props.filled ? props.theme.textPrimary : 'white'};
   border: ${props => props.filled ? 'none' : `2px solid ${props.theme.textPrimary}`};
@@ -8,7 +8,14 @@ export const Button = styled.button`
   font-size: 1rem;
   line-height: 3rem;
   padding: 4px 36px;
+  width: max-content;
   height: 3rem;
+  transition: all .2s ease 0s;
+
+  &:hover {
+    transform: translate(0, -5px);
+
+  }
 `
 
 export default Button
