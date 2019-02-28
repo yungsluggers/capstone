b13d3d13eb3a78bee5fb02d2d5b41929c2874f45
Michael Rascati <rascatimichael@gmail.com>
2019-02-27 22:18:11 -0500

    button row abstracted to a component

---
+++ app/src/components/Button/ButtonRow.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const ButtonRow = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  width: 100%;
+  margin-top: 50px;
+
+  Button {
+    margin: 50px 10px 0 10px;
+  }
+`
+
+export default ButtonRow
