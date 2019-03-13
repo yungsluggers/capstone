fb689be6c65f97d8e6b92932581e02f77f5bc1e2
kevinaltschuler <kevinaltschuler@gmail.com>
2019-03-13 17:20:50 -0400

    cors fix

--- serversrc/server.js
+++ serversrc/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	next();
 });
 
