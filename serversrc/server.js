50e0fc971ab43f2c71ee20435b64cb63e57ff5be
kevinaltschuler <kevinaltschuler@gmail.com>
2019-03-13 17:14:20 -0400

    post to cors

--- serversrc/server.js
+++ serversrc/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Accept');
 	next();
 });
