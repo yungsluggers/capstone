4ca982e6e809c913af63da59ac4620bd4dce8bcc
Michael Rascati <rascatimichael@gmail.com>
2019-04-15 16:37:08 -0400

    collapsable navbar with scrolling

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -18,6 +18,21 @@ const Icon = styled.a`
   }
 `
 
+const Subtitle = styled.div`
+  text-transform: lowercase;
+`
+
+const Signature = styled.div`
+  display: flex;
+  align-items: flex-start;
+  text-transform: lowercase;
+  transform: rotate(180deg);
+  writing-mode: vertical-rl;
+  display: flex;
+  width: 100%;
+  align-items: flex-end;
+`
+
 const Title = styled.div`
   display: flex;
   flex-flow: column nowrap;
@@ -35,46 +50,97 @@ const Title = styled.div`
   }
 `
 
-const Subtitle = styled.div`
-  text-transform: lowercase;
-`
-
-const Signature = styled.div`
-  display: flex;
-  align-items: flex-start;
-  text-transform: lowercase;
-  transform: rotate(180deg);
-  writing-mode: vertical-rl;
-  display: flex;
-  width: 100%;
-  align-items: flex-end;
-`
-
 const Navbar = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  background: white;
+  width: 100%;
+  overflow: hidden;
+  padding: 1.5rem 0;
+  z-index: 1;
+`
+
+const ContentWrapper = styled.div`
   display: flex;
   flex-flow: row nowrap;
 `
 
-const NavTitle = styled(Title)`
+const CollapsedNavbar = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  position: fixed;
+  top: -210px;
+  overflow: hidden;
+  padding: 1rem 0 0.6rem 0;
+  z-index: 1;
+  background: white;
+  width: 100%;
+  border-bottom: 2px solid ${props => props.theme.textPrimary};
+  transition: 0.5s ease-out;
+`
+
+const CollapsedTitle = styled(Title)`
   span {
     font-size: 1.375rem;
     letter-spacing: initial;
-    width: initial;
+    line-height: initial;
     height: initial;
+    transition: 0.5s ease-out;
   }
 `
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.navRef = React.createRef()
+    this.titleRef = React.createRef()
+
+    this.state = {
+      navbarStyle: {},
+      sigStyle: {}
+    }
+
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll(event) {
+    if (document.documentElement.scrollTop > '300') {
+      // Styles when Collapsed
+
+      this.setState({
+        navbarStyle: {
+          top: '0'
+        }
+      })
+    } else {
+      // Default styles
+
+      this.setState({
+        navbarStyle: {
+          top: '-210px'
+        }
+      })
+    }
+  }
+
   render() {
     return (
       <Container>
-        <div className={this.props.narrow ? 'l-container' : null}>
-          <Navbar>
-            <NavTitle>
+        <Navbar>
+          <ContentWrapper className={this.props.narrow ? 'l-container' : null}>
+            <Title>
               <span>Kaleido-</span>
               <span>saur</span>
-            </NavTitle>
-
+            </Title>
             <Icon
               onClick={() => {
                 this.props.aboutRef.current.scrollIntoView({
@@ -86,7 +152,6 @@ class Header extends Component {
             >
               <HelpIcon />
             </Icon>
-
             <Icon
               onClick={() => {
                 this.props.galleryRef.current.scrollIntoView({
@@ -98,16 +163,44 @@ class Header extends Component {
             >
               <GalleryIcon />
             </Icon>
-          </Navbar>
+          </ContentWrapper>
+        </Navbar>
 
-          <Title>
-            <span>Kaleido-</span>
-            <span>saur</span>
-          </Title>
+        <CollapsedNavbar style={this.state.navbarStyle}>
+          <ContentWrapper className={this.props.narrow ? 'l-container' : null}>
+            <CollapsedTitle>
+              <span>Kaleido-</span>
+              <span>saur</span>
+            </CollapsedTitle>
+            <Icon
+              onClick={() => {
+                this.props.aboutRef.current.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'center',
+                  inline: 'center'
+                })
+              }}
+            >
+              <HelpIcon />
+            </Icon>
+            <Icon
+              onClick={() => {
+                this.props.galleryRef.current.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'center',
+                  inline: 'center'
+                })
+              }}
+            >
+              <GalleryIcon />
+            </Icon>
+          </ContentWrapper>
+        </CollapsedNavbar>
 
+        <div className={this.props.narrow ? 'l-container' : null}>
           <Subtitle>Abstract art generator</Subtitle>
 
-          <Signature narrow>
+          <Signature style={this.state.sigStyle}>
             Capstone 2019 <br />
             Northeastern University
           </Signature>
