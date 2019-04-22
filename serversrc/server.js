3238b9e2c51c50de5316e079bcef421b2fefd52d
kevinaltschuler <kevinaltschuler@gmail.com>
2019-04-22 15:43:34 -0400

    added extened true to serverjs

--- serversrc/server.js
+++ serversrc/server.js
@@ -16,8 +16,8 @@ const helmet = require('helmet')
 const { execFile } = require('child_process')
 
 app.use(helmet())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '10mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
