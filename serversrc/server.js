e3a7697f46a91967edd785d46cc1378cb125e2f5
kevinaltschuler <kevinaltschuler@gmail.com>
2019-04-23 15:27:47 -0400

    spawn dnot defind

--- serversrc/server.js
+++ serversrc/server.js
@@ -14,7 +14,7 @@ const tempWrite = require('temp-write')
 
 //var credentials = { key: privateKey, cert: certificate }
 const helmet = require('helmet')
-const { execFile, exec } = require('child_process')
+const { execFile, exec, spawn } = require('child_process')
 
 app.use(helmet())
 app.use(bodyParser.json({ limit: '10mb', extended: true }))
